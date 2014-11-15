25cb19dcc99b328fdcb214f460b0a45f97644cb3
John Wilkinson <jcwilk@gmail.com>
2014-11-15 10:25:02 +0000

    Add sliders for magic numbers! Exciting.

--- app/controllers/client_controller.rb
+++ app/controllers/client_controller.rb
@@ -1,3 +1,10 @@
 class ClientController < ApplicationController
-  def new; end
+  def new
+    @slider_vars = {
+      turnDuration: 5000,
+      preventDoubleInputDelay: 100,
+      waitAfterInputBeforeTurnEndDelay: 200,
+      animationDuration: 100
+    }
+  end
 end
