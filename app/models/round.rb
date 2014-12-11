e0f7f8d77e2324f47af1250f54efe66d5b0cb10e
John Wilkinson <jcwilk@gmail.com>
2014-12-11 10:39:36 +0000

    Move some logic into instance rounds
    Also fix a bug, perhaps introduced here? where the round was off by 1 in the js

--- app/models/round.rb
+++ app/models/round.rb
@@ -1,7 +1,10 @@
-module Round
+class Round
   ROUND_DURATION = 5 #seconds
 
   class << self
+    delegate :waiting_players, :player_data, :current_data,
+      to: :current_round
+
     def add_move(player_uuid, move)
       player_data[player_uuid] = move
       Player.mark_active(player_uuid)
@@ -12,18 +15,21 @@ module Round
         puts "remaining - #{remaining_players.inspect}"
 
         if remaining_players.present?
+          puts 'peeps remaining!'
           RoomEventsController.publish('/room_events/waiting', {player_uuid: player_uuid,current_round: current_number}.to_json)
         else
+          puts 'advancin'
           advance
         end
       else
+        puts 'not active'
         @active = true
         advance
       end
     end
 
     def advance
-      complete
+      current_round.complete
 
       if recent_activity?
         start
@@ -33,24 +39,25 @@ module Round
       end
     end
 
-    def waiting_players
-      @waiting_players ||= Set.new([])
-    end
-
-    def current_data
-      {}.tap do |data|
-        data[:players] = recent_player_data
-        data[:current_round] = current_number
-      end
-    end
-
     def reset #testing hax
-      @player_data = @waiting_players = @current_number = nil
-      @last_recent_players = @active = nil
+      @all = nil
+      @active = nil
       Player.reset
     end
 
+    def current_round
+      all.last
+    end
+
+    def current_number
+      current_round.index
+    end
+
     private
+
+    def all
+      @all ||= [new]
+    end
 
     def active?
       !!@active
@@ -61,25 +68,12 @@ module Round
     end
 
     def current_number
-      val = @current_number
-      !!val ? val.to_i : 0
-    end
-
-    def player_data
-      @player_data ||= {}
-    end
-
-    def recent_player_data
-      #TODO: not very efficient
-      player_data.select {|k,v| Player.recent_uuid?(k) }
+      all.length-1
     end
 
     def start
-      puts 'starting new round!'
-      @current_number = current_number+1
-      RoomEventsController.publish('/room_events/advance', current_data.to_json)
+      all << new(current_round)
       curr_num = current_number
-      @waiting_players = nil
       puts 'setting timer...'
       EM.add_timer(ROUND_DURATION) do
         if Round.current_data[:current_round] == curr_num
@@ -90,18 +84,42 @@ module Round
         end
       end
     end
+  end
 
-    def complete
-      recent_players = Player.recent.map(&:uuid)
-      @last_recent_players||= recent_players
-      lost_players = @last_recent_players - recent_players
+  attr_reader :participants, :waiting_players, :index, :player_data
 
-      lost_players.each do |uuid|
-        RoomEventsController.publish('/room_events/waiting', {player_uuid: uuid,current_round: current_number}.to_json)
-        @waiting_players.delete(uuid) if @waiting_players
-      end
-      #TODO: so race condition. wow.
-      @last_recent_players = recent_players
+  def initialize(father = nil)
+    @waiting_players = []
+    if father
+      @index = father.index + 1
+      @player_data = father.player_data.dup
+      @last_participants = father.participants
+    else
+      @index = 0
+      @player_data = {}
+      @last_participants = Player.recent
     end
+  end
+
+  def current_data
+    {}.tap do |data|
+      data[:players] = recent_player_data
+      data[:current_round] = index
+    end
+  end
+
+  def recent_player_data
+    #TODO: make this draw off participants
+    player_data.select {|k,v| Player.recent_uuid?(k) }
+  end
+
+  def participants
+    @participants || Player.recent
+  end
+
+
+  def complete
+    @participants = participants
+    RoomEventsController.publish('/room_events/advance', current_data.to_json)
   end
 end
