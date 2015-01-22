374179a6712bfde73160a5c84f4784cb412f29ee
John Wilkinson <jcwilk@gmail.com>
2015-01-22 23:10:06 +0000

    force the port, prod bug

--- config/initializers/canonical_host.rb
+++ config/initializers/canonical_host.rb
@@ -1,9 +1,11 @@
 module Divided
   CANONICAL_HOST = if Rails.env.test?
-      'example.com'
+      'example.com:80'
     elsif Rails.env.development?
       'localhost:3000'
     else
-      'divided.herokuapp.com' #TODO: make this an env var
+      #TODO: figure out why this needs a port
+      # it defaults to 0 otherwise for some reason
+      'divided.herokuapp.com:80' #TODO: make this an env var
     end
 end
