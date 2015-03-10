08238bfcd1ea7988849678da09493ad1e35c66c6
John Wilkinson <jcwilk@gmail.com>
2015-03-10 03:17:26 +0000

    Fix bug where miss glows of first round

--- app/controllers/client_controller.rb
+++ app/controllers/client_controller.rb
@@ -8,5 +8,11 @@ class ClientController < ApplicationController
     @player_uuid = player.uuid #SecureRandom.urlsafe_base64(8)
     #@last_round = Round.last_round.currend_data.to_json
     @first_move = Round.current_data[:players][@player_uuid].to_json
+
+    #TODO: would be great if the above variables drew from this
+    # or another hal object
+    @participant = DV::Representers::Participant.render(
+      Participant.new(round: Round.current_round, player: player)
+    )
   end
 end
--- app/grape/dv/participant.rb
+++ app/grape/dv/participant.rb
@@ -10,6 +10,10 @@ module DV
         end
 
         get do
+          #TODO: make this specific to a round
+          # will require returning the current round and next round
+          # in the round advancement event to the client
+          # since the next round will have all the next moves
           player = ::Player.alive_by_uuid(params[:id])
           if player.nil?
             error! 'Participant not found!', 404
